04dc115d7d8a1043cb04cf82df16bbecf5a8ac46
sirjandrn25 <56628702+sirjandrn25@users.noreply.github.com>
2024-03-04 08:25:12 +0545

    backend setup

---
+++ apps/server/src/index.ts
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+// import ModuleRouter from "./modules/root.route";
+// import UserModel from "./user.model";
+const bodyParser = require("body-parser");
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT ?? 8000;
+
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
+
+db().catch((err) => console.log(err));
+
+async function db() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/hotel-mgt");
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+// app.use("/api", ModuleRouter);
+app.get("/customers", (req: Request, res: Response) => {});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
---
+++ apps/server/src/middlewares/error-handler.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+
+const errorHandler = (
+  error: any,
+  req: Request,
+  res: Response,
+  next: Function
+) => {
+  error.statusCode = error.statusCode || 500;
+
+  res.status(error.statusCode).send({
+    status: error.statusCode,
+    message: error.message,
+  });
+};
+
+export const asyncErrorHandler = (func: Function) => {
+  return (req: Request, res: Response, next: Function) =>
+    func(req, res, next).catch((error: any) => {
+      // if (!error?.code)
+      //   return res?.status(500).send({ message: "Internal Server Error" });
+      if (error?.code === "P2002") {
+        const target = error?.meta?.target[0];
+
+        return res.status(400).send({
+          [target]: "unique constraint failed !!",
+        });
+      }
+
+      next(error);
+    });
+};
+export default errorHandler;
